267ba28fd5a7e0e5a98a3baba2a0187051536399
abu bakar <abubakar850772@gmail.com>
2025-05-25 12:45:29 +0600

    feat(auth): add user registration and login validation schemas

---
+++ src/modules/auth/auth.validation.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const userRegistrationValidationSchema = z
+    .object({
+        username: z.string().trim().min(3, "Username must be at least 3 characters long"),
+        email: z.string().email("Invalid email address"),
+        password: z
+            .string()
+            .min(6, "Password must be at least 6 characters long")
+            .regex(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+                "Password must contain uppercase, lowercase, number, and symbol",
+            ),
+        confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "",
+        path: ["confirmPassword"],
+    });
+
+export const userLoginValidationSchema = z.object({
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters long"),
+});
