2ac303b3744f836bc3b760aa165c89d9e0dce03e
abu bakar <abubakar850772@gmail.com>
2025-05-26 21:18:59 +0600

    chore: normalize line endings

--- src/config/index.ts
+++ src/config/index.ts
@@ -5,5 +5,14 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 export const config = {
     port: process.env.PORT || 3000,
+    node_env: process.env.NODE_ENV || "development",
     db_uri: process.env.DB_URI,
+    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+    jwt_access_secret: process.env.ACCESS_TOKEN_SECRET,
+    jwtExpiry: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    user_email: process.env.USER_EMAIL,
+    user_password: process.env.USER_PASSWORD,
+    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinary_api_key: process.env.CLOUDINARY_API_KEY,
+    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET,
 };
